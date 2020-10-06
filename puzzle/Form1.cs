925df85f63e5ed8cec1393443475f645d0d9cd41
keathal <jemovey@gmail.com>
2020-10-06 21:17:59 +0700

    Add files via upload

---
+++ puzzle/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace puzzle
+{
+    public partial class Form1 : Form
+    {
+        Point emptyPoint;
+        Game game = new Game();
+        Timer timer=new Timer();
+        public Form1()
+        {
+            emptyPoint.X = game.settings.width* ((int)Math.Sqrt(game.settings.amount)-1);
+            emptyPoint.Y = game.settings.width * ((int)Math.Sqrt(game.settings.amount)-1);
+            InitializeComponent();
+            timer.Interval = 3000;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            foreach (Button  btn in panel1.Controls)
+            {
+                btn.Enabled = true;
+            }
+            Image original = Image.FromFile(GetImage());
+            game.cropImage(original, 420);
+            addImagesToButtons(game.suffle(game.settings.amount), game.settings.amount);
+        }
+
+         void addImagesToButtons(Image[] curImages, int count)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < count-1; i++)
+            {
+                list.Add(i);
+            }
+            int j = 0;
+            foreach(Button btn in panel1.Controls)
+            {
+                if(j<list.Count)
+                {
+                    btn.Image = curImages[list[j]];
+                    j++;
+                }
+            }
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if(game.moveButton(btn.Location.X, btn.Location.Y))
+            {
+                Point swap = btn.Location;
+                btn.Location = emptyPoint;
+                emptyPoint = swap;
+                string text = "";
+                for (int i = 0; i < game.settings.amount; i++)
+                {
+                    text += " Puzzle " + i + " goes to " + game.curPuzzles[i].id;
+                }
+               // t_test.Text = text;
+                l_steps.Text = "Шагов сделано: " + game.settings.steps;
+                if (game.checkValid() != "")
+                    t_test.Text = game.checkValid();
+            } 
+            
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            foreach (Button btn in panel1.Controls)
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderSize = 2;
+                if (game.checkButton(btn.Location.X, btn.Location.Y))
+                    btn.FlatAppearance.BorderColor = Color.Green;
+                else
+                    btn.FlatAppearance.BorderColor = Color.Red;
+
+                timer.Enabled = true;
+                timer.Tick += Timer_Tick;
+
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            foreach (Button btn in panel1.Controls)
+            {
+                btn.FlatAppearance.BorderSize = 0;
+            }
+            timer.Enabled = false;
+        }
+
+        static string GetImage()
+        {
+            List<string> img = new List<string>();
+            img.Add(@"D:\puzzle\puzzle\original.JPG");
+            img.Add(@"D:\puzzle\puzzle\2.jpeg");
+            img.Add(@"D:\puzzle\puzzle\3.jpg");
+            img.Add(@"D:\puzzle\puzzle\4.jpg");
+            img.Add(@"D:\puzzle\puzzle\5.jpg");
+            img.Add(@"D:\puzzle\puzzle\6.png");
+            Random rnd = new Random();
+            return (img[rnd.Next(0, img.Count - 1)]);
+        }
+    }
+}
---
+++ puzzle/Game.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Collections;
+
+namespace puzzle
+{
+    class Game
+    {
+        public Settings settings = new Settings();
+        public Puzzle[] puzzles;
+        public Puzzle[] curPuzzles;
+        public int maxX { get; set; }
+        ArrayList images = new ArrayList();
+
+        public Game()
+        {
+            puzzles = new Puzzle[settings.amount];
+            curPuzzles = new Puzzle[settings.amount];
+            maxX = settings.width * (int)Math.Sqrt(settings.amount);
+        }
+        public ArrayList cropImage(Image img, int width)
+        {
+            Bitmap bmp = new Bitmap(width, width);
+            Graphics graphic = Graphics.FromImage(bmp);
+            graphic.DrawImage(img, 0, 0, width, width);
+            graphic.Dispose();
+
+            int movr = 0, movd = 0;
+            for (int x = 0; x < settings.amount; x++)
+            {
+                Bitmap piece = new Bitmap(settings.width, settings.width);
+                for (int i = 0; i < settings.width; i++)
+                    for (int j = 0; j < settings.width; j++)
+                    {
+                        piece.SetPixel(i, j, bmp.GetPixel(i + movr, j + movd));
+                    }
+
+                images.Add(piece);
+                puzzles[x] = new Puzzle(piece, x);
+                puzzles[x].X = x % (int)Math.Sqrt(settings.amount);
+                puzzles[x].Y = x / (int)Math.Sqrt(settings.amount);
+                movr += settings.width;
+                if (movr == maxX)
+                {
+                    movr = 0;
+                    movd += settings.width;
+                }
+            }
+            return images;
+        }
+        public Image[] suffle(int count)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(i);
+            }
+            Random rnd = new Random();
+            list = list.OrderBy(x => rnd.Next()).ToList();
+            Image[] returnImgs = new Image[puzzles.Length];
+            foreach (int i in list)
+            {
+                curPuzzles[i] = puzzles[list[i]];
+                curPuzzles[i].X = i % (int)Math.Sqrt(settings.amount);
+                curPuzzles[i].Y = i / (int)Math.Sqrt(settings.amount);
+                returnImgs[i] = curPuzzles[i].img;
+            }
+            curPuzzles[settings.amount - 1].isempty = true;
+            curPuzzles[settings.amount - 1].X = (int)Math.Sqrt(settings.amount) - 1;
+            curPuzzles[settings.amount - 1].Y = (int)Math.Sqrt(settings.amount) - 1;
+            return returnImgs;
+        }
+        public Image[] getImages()
+        {
+            Image[] returnImgs = new Image[puzzles.Length];
+            int i = 0;
+            foreach (Puzzle item in curPuzzles)
+            {
+                if (item != null)
+                    returnImgs[i] = item.img;
+                else
+                    returnImgs[i] = null;
+                i++;
+            }
+            return returnImgs;
+        }
+        public string checkValid()
+        {
+            int count = 0;
+
+            for (int i = 0; i < settings.amount; i++)
+            {
+                if (i == curPuzzles[i].id)
+                    count++;
+            }
+
+            if (count >= settings.amount-1)
+                return "you win!";
+            else
+                return "";
+        }
+        public bool moveButton(int x, int y)
+        {
+            x = x / settings.width;
+            y = y / settings.width;
+            int id = y * 3 + x;
+            Puzzle item = curPuzzles[id];
+            Puzzle empty = curPuzzles.Where(b => b.isempty == true).FirstOrDefault();
+            if (item != null && empty!=null)
+            {
+                if (((x == empty.X - 1 || x == empty.X + 1) &&
+                y == empty.Y)
+                || ((y == empty.Y - 1 || y == empty.Y + 1) &&
+                x == empty.X))
+                {
+                    int emptyID = empty.Y * (int)Math.Sqrt(settings.amount) + empty.X;
+                    curPuzzles[id] = empty;
+                    curPuzzles[id].X = x; curPuzzles[id].Y = y;
+
+                    curPuzzles[emptyID] = item;
+                    curPuzzles[emptyID].X = empty.X; curPuzzles[emptyID].Y = empty.Y;
+                    settings.steps++;
+                    return true;
+                }
+                else
+                    return false;
+            }
+                
+            else return false;
+        }
+        public bool checkButton(int x, int y)
+        {
+            x = x / settings.width;
+            y = y / settings.width;
+            int id = y * 3 + x;
+            if (id == curPuzzles[id].id)
+                return true;
+            else
+                return false;
+        }
+    }
+}
